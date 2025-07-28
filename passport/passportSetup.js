9656e74666175a5825012dd638a497d07f4e4ebe
Arnaud Sanchez <arnaudsanchez1816@gmail.com>
2025-07-28 23:09:52 +0200

    Update passport setup to use queries script

--- passport/passportSetup.js
+++ passport/passportSetup.js
@@ -1,32 +1,27 @@
 const passport = require("passport")
 const passportLocal = require("passport-local").Strategy
 const bcrpyt = require("bcryptjs")
-const pool = require("../db/pool")
+const db = require("../db/queries")
+
+const INCORRECT_USERNAME_PASSWORD_MESSAGE = "Incorrect e-mail or password."
 
 passport.use(
     new passportLocal(async (username, password, done) => {
         // Verify user
         try {
-            const { rows } = await pool.query(
-                `
-            SELECT *
-            FROM users
-            WHERE email = $1;
-            `,
-                [username]
-            )
-
-            const user = rows[0]
+            const user = await db.getUserFromEmail({ email: username })
 
             if (!user) {
                 return done(null, false, {
-                    message: "Incorrect username/email",
+                    message: INCORRECT_USERNAME_PASSWORD_MESSAGE,
                 })
             }
 
             const passwordMatch = await bcrpyt.compare(password, user.password)
             if (!passwordMatch) {
-                return done(null, false, { message: "Incorrect password" })
+                return done(null, false, {
+                    message: INCORRECT_USERNAME_PASSWORD_MESSAGE,
+                })
             }
 
             return done(null, user)
@@ -42,11 +37,7 @@ passport.serializeUser((user, done) => {
 
 passport.deserializeUser(async (id, done) => {
     try {
-        const { rows } = await pool.query(
-            "SELECT * FROM users WHERE id = $1;",
-            [id]
-        )
-        const user = rows[0]
+        const user = await db.getUserFromId({ id })
         done(null, user)
     } catch (error) {
         done(error)
